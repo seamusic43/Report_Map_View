7bbb503f2dace3aeabfe4b8ed4bbda0fcdd024a4
seamusic <dev.seamusic@gmail.com>
2024-01-20 09:56:36 +0900

    add model

--- models/store_info.model.js
+++ models/store_info.model.js
@@ -18,10 +18,15 @@ module.exports = (sequelize) => {
             },
             comment: '사이트 고유번호'
         },
+        store_id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+            unique: true,
+            comment: '사이트 체험 고유 번호 '
+        },
         title: {
             type: DataTypes.CHAR(100),
             allowNull: false,
-            unique: true,
             comment: '체험 타이틀'
         },
         location: {
@@ -29,21 +34,54 @@ module.exports = (sequelize) => {
             comment: '지역명'
         },
         category: {
-            type: DataTypes.ENUM(['BLOG', 'DELIVERY', 'REPORT']),
+            type: DataTypes.ENUM(['VISIT', 'DELIVERY', 'REPORT']),
             allowNull: false,
             comment: '카테고리 타입'
         },
+        post_type: {
+            type: DataTypes.ENUM(['BLOG', 'INSTA']),
+            allowNull: false,
+            comment: '포스팅 타입',
+        },
+        address: {
+            type: DataTypes.CHAR(255),
+            allowNull: false,
+            comment: '주소',
+        },
+        lat: {
+            type: DataTypes.FLOAT,
+            comment: '위도',
+        },
+        long: {
+            type: DataTypes.FLOAT,
+            comment: '경도'
+        },
+        apply_started_at: {
+            type: DataTypes.DATEONLY,
+            comment: '신청 시작일'
+        },
+        apply_ended_at: {
+            type: DataTypes.DATEONLY,
+            comment: '신청 종료일'
+        },
+        selected_at: {
+            type: DataTypes.DATEONLY,
+            comment: '당첨 발표일'
+        },
+        posting_started_at: {
+            type: DataTypes.DATEONLY,
+            comment: '포스팅 시작일'
+        },
+        posting_ended_at: {
+            type: DataTypes.DATEONLY,
+            comment: '포스팅 종료일'
+        },
+        is_display: {
+            type: DataTypes.BOOLEAN,
+            comment: '노출여부'
+        }
     }, {
         timestamps: true,
-        charset: 'utf8',
-        indexes: [
-            {
-                name: 'site_name',
-                type: 'UNIQUE',
-                unique: true,
-                fields: ['name'],
-            }
-        ]
-
+        charset: 'utf8'
     })
 }
